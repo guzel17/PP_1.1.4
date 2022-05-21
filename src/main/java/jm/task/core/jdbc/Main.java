3c8fd46d8fdfa217f67a1c2155ca7f2bb57b58a9
guzel17 <guzelka17@gmail.com>
2022-05-22 03:34:20 +0500

    commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,9 +3,6 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,7 +12,7 @@ public class Main {
         userService.saveUser("Ivan", "Sidorov", (byte) 30);
         userService.saveUser("Anna", "Lebedeva", (byte) 28);
         userService.removeUserById(1);
-        List<User> userList = userService.getAllUsers();
+        userService.getAllUsers();
         userService.cleanUsersTable();
         userService.dropUsersTable();
     }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,13 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.CriteriaQuery;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -9,34 +15,78 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
-
     @Override
     public void createUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            String str = "CREATE TABLE IF NOT EXISTS users (" +
+                    "id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
+                    "name VARCHAR (35) NOT NULL, " +
+                    "lastName VARCHAR (35) NOT NULL, " +
+                    "age INT NOT NULL)";
+            session.createSQLQuery(str).executeUpdate();    //
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createNativeQuery("DROP TABLE IF EXISTS users ").executeUpdate();
+            transaction.commit();
+        }
+        catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+                session.save(new User(name, lastName, age));
+                transaction.commit();
+                System.out.println("User с именем – " + name + " добавлен в базу данных");
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.delete(session.get(User.class, id));
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+            try (Session session = Util.getSessionFactory().openSession()) {
+                Transaction transaction = session.beginTransaction();
+                users = session.createQuery("FROM User").getResultList();
+                transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createNativeQuery("TRUNCATE TABLE users").executeUpdate();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
 
     }
 }
